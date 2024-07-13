f96100ff58fb54fd19fe85d38d3bf55d836ad730
Barto <bartekjanik45@gmail.com>
2024-07-13 16:26:33 +0200

    feat: SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <></>
+import { SearchBar } from "./components/SearchBar"
+
+const App = () => {
+  const wrapperClasses =
+    "bg-slate-800 gap-y-3 h-screen flex flex-col items-center justify-center text-slate-50"
+
+  return (
+    <div className={wrapperClasses}>
+      <SearchBar />
+    </div>
+  )
 }
 
 export default App
