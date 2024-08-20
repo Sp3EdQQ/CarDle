e6caacaae04aeb5d73399ca96756a2ad3ee17376
Barto <bartekjanik45@gmail.com>
2024-08-20 21:34:33 +0200

    feat: generating random car function

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { CarAttributes } from "./components/CarAttributes"
 import { ColorsLegend } from "./components/ColorsLegend"
 import MainLogo from "./assets/images/Logo/logo-no-background.svg?react"
 import { useState } from "react"
-import { GetCarInfo } from "./types/carInfo.ts"
+import { GetCarInfo } from "./types/carInfo"
+import { generateRandomCar } from "./utils/generateRandomCar.tsx"
 
 const App = () => {
   const [carInfo, setCarInfo] = useState<GetCarInfo[]>([])
+
+  const randomCar = generateRandomCar()
+  console.log(randomCar)
+
   return (
     <div className="bg-[url('src/assets/images/Background/bg.webp')] bg-fixed bg-cover min-h-dvh gap-y-5 flex flex-col items-center text-slate-50">
       <MainLogo className="size-1/3 my-3 " />
---
+++ src/utils/generateRandomCar.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react"
+import { GetCarInfo } from "../types/carInfo.ts"
+import { Trim } from "../types/trims.ts"
+import { useParamMutation } from "../hooks/useParamMutation.ts"
+import { useMakes } from "../hooks/useMakes.ts"
+import { Model } from "../types/models.ts"
+import { useModelsMutation } from "../hooks/useModelsMutation.ts"
+import { useTrimMutation } from "../hooks/useTrimMutation.ts"
+
+export const generateRandomCar = () => {
+  const [selectedMake, setSelectedMake] = useState("")
+  const [selectedModel, setSelectedModel] = useState("")
+  const [selectedTrim, setSelectedTrim] = useState<Trim | undefined>()
+  const [randomCar, setRandomCar] = useState<GetCarInfo[]>([])
+
+  const paramMutation = useParamMutation(setRandomCar)
+
+  const { data: makesResponse } = useMakes()
+  const getRandomMake = () => {
+    const makes = makesResponse?.data.map(({ name }) => name) ?? []
+    const randomMakesIndex = Math.floor(Math.random() * makes.length)
+    return makes[randomMakesIndex]
+  }
+
+  const getRandomModel = (models: Model[]) => {
+    const modelsNames = models?.map(({ name }) => name)
+    const randomModelsIndex = Math.floor(Math.random() * modelsNames?.length)
+    const randomModel = modelsNames?.[randomModelsIndex]
+
+    if (randomModel) setSelectedModel(randomModel)
+  }
+
+  const getRandomTrim = (trims: Trim[]) => {
+    const randomTrimIndex = Math.floor(Math.random() * trims?.length)
+    const randomTrim = trims[randomTrimIndex]
+    setSelectedTrim(randomTrim)
+  }
+
+  const modelsMutation = useModelsMutation(getRandomModel)
+  const trimMutation = useTrimMutation(getRandomTrim)
+
+  useEffect(() => {
+    const randomMake = getRandomMake()
+    if (randomMake) {
+      setSelectedMake(randomMake)
+      modelsMutation.mutate(randomMake)
+    }
+  }, [makesResponse])
+
+  useEffect(() => {
+    if (selectedMake && selectedModel) {
+      trimMutation.mutate({ make: selectedMake, model: selectedModel })
+    }
+  }, [selectedModel])
+
+  useEffect(() => {
+    if (selectedTrim?.id) paramMutation.mutate(selectedTrim?.id)
+  }, [selectedTrim])
+  return randomCar
+}
